0450a8ee3b8db1454524a663dfeb64d7a3128603
Pedro de Mesquita Bittencourt <djojoo@gmail.com>
2024-02-05 11:10:27 -0300

    Feat: Point and Line class, draw_line methods

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ class Window:
         self.__root.title("Maze")
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.__canva = Canvas(width=self.width, height=self.height)
-        self.__canva.pack()
+        self.__canvas = Canvas(width=self.width, height=self.height)
+        self.__canvas.pack()
         self.__is_running = False
         
     def redraw(self):
@@ -25,10 +25,37 @@ class Window:
     def close(self):
         self.__is_running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+class Point:
+    def __init__(self, x, y):
+        #X is horizontal coordinate
+        #x=0 is the left of the screen
+        self.x = x
+        #Y is vertical coordinate
+        #y=0 is the top of the screen
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2)
+        canvas.pack()
+
+
 
 
 def main():
     win = Window(800, 600)
+    point_1 = Point(50, 50)
+    point_2 = Point(200, 50)
+    line_1 = Line(point_1, point_2)
+    win.draw_line(line_1, "black")
     win.wait_for_close()
 
 main()
